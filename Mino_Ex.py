940d644a95f3faea28fd35d0b55c98109837eddf
Sannidhya Dasgupta <72875652+Sannidhya4396@users.noreply.github.com>
2020-10-14 21:56:35 +0530

    Initial Commit

---
+++ Mino_Ex.py
@@ -0,0 +1,14 @@
+
+a = int(input("Enter the number of items you want\n"))
+b = int(input("Enter 1 for list, 2 for dict, 3 for set\n"))
+list1 = [i for i in range(a) if b == 1]
+if b == 1:
+    print(list1)
+
+dict1 = {i: f"item {i}" for i in range(a) if b == 2}
+if b == 2:
+    print(dict1)
+
+set1 = {i for i in range(a) if b == 3}
+if b == 3:
+    print(set1)
