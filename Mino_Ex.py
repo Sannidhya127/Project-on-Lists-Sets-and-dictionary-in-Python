da6d271ff6180b7cfd1bcc07e54fc0d137a124e0
Sannidhya Dasgupta (Secondary Account) <71244547+cod-e-sd100@users.noreply.github.com>
2020-10-15 15:00:22 +0530

    Added some grammatical changes in variable b

--- Mino_Ex.py
+++ Mino_Ex.py
@@ -1,6 +1,6 @@
 
 a = int(input("Enter the number of items you want\n"))
-b = int(input("Enter 1 for list, 2 for dict, 3 for set\n"))
+b = int(input("Enter 1 for list, 2 for dictionary and 3 for set\n"))
 list1 = [i for i in range(a) if b == 1]
 if b == 1:
     print(list1)
